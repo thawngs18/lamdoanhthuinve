8ae46d919249bafcf65c700983e2cae56387fcf2
Thắng Phan Đức <apgpdt10@gmail.com>
2024-11-26 23:34:00 +0700

    Add project files.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Bisque;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 10);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(752, 73);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(746, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MAN HINH";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(134, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(499, 403);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(21, 90);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(107, 402);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 613);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+
+    public partial class Form1 : Form
+    {
+        public static string GetAsciiString(int[] numbers)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int number in numbers)
+            {
+                sb.Append((char)number);
+            }
+
+            return sb.ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            khoitaosoghe(11, 15);
+            khoitaoday(11, 1);
+        }
+
+        private void khoitaoday(int v1, int v2)
+        {
+            int x, y = 16, kc = 30, d=65;
+            for (int i = 0; i < v1; i++)
+            {
+                x = 11;   
+                for (int j = 0; j < v2; j++)
+                {
+                    Button btnDay = new Button();
+                    btnDay.Location = new System.Drawing.Point(x, y);
+                    btnDay.Size = new System.Drawing.Size(30, 23);
+                    btnDay.Text = ((char)d).ToString();
+                    btnDay.BackColor = Color.White;
+                    panel2.Controls.Add(btnDay);
+                    x += kc;
+                }
+                y += kc;
+                d++;
+            }
+        
+        }
+
+        private void khoitaosoghe(int v1, int v2)
+        {
+            int x, y = 16,kc = 30,d;
+            for (int i = 0; i < v1; i++) {
+                x = 11; 
+                d = 1;
+                
+
+
+                for (int j = 0; j < v2; j++)
+                {
+                    Button btnGhe = new Button();
+                    btnGhe.Location = new System.Drawing.Point(x, y);
+                    btnGhe.Size = new System.Drawing.Size(30, 23);
+                    btnGhe.Text = d++.ToString();
+                    btnGhe.BackColor = Color.White;
+                    panel1.Controls.Add(btnGhe);
+                    x += kc;                  
+                }
+                y += kc;
+            } 
+        }
+    }
+}
