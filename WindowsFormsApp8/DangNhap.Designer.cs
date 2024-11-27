61db18dde4df816059d1d6894df21940bc78f58f
Tài Nguyễn Tấn <nguyentantai220804@gmail.com>
2024-11-27 11:14:27 +0700

    dang la 2

--- WindowsFormsApp8/DangNhap.Designer.cs
+++ WindowsFormsApp8/DangNhap.Designer.cs
@@ -33,58 +33,70 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(136, 103);
+            this.textBox1.Location = new System.Drawing.Point(37, 144);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 38);
+            this.textBox1.Size = new System.Drawing.Size(295, 38);
             this.textBox1.TabIndex = 1;
-
             // 
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(125, 55);
+            this.textBox2.Location = new System.Drawing.Point(37, 52);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 37);
+            this.textBox2.Size = new System.Drawing.Size(295, 37);
             this.textBox2.TabIndex = 1;
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(42, 158);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 37);
+            this.label3.Size = new System.Drawing.Size(94, 37);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Mat Khau";
+            this.label3.Text = "Mat Khau:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(19, 55);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 37);
+            this.label2.Size = new System.Drawing.Size(94, 37);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Tai Khoan";
+            this.label2.Text = "Tai Khoan:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(34, 94);
+            this.panel1.Location = new System.Drawing.Point(12, 98);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(331, 287);
+            this.panel1.Size = new System.Drawing.Size(364, 261);
             this.panel1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(74, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Dang Nhap";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // DangNhap
             // 
@@ -106,5 +118,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
