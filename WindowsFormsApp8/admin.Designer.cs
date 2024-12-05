2f0f5b2ff49c1c57de2011958f540071b7f53a0d
Thắng Phan Đức <apgpdt10@gmail.com>
2024-12-05 22:16:55 +0700

    bat dau lai admin

--- WindowsFormsApp8/admin.Designer.cs
+++ WindowsFormsApp8/admin.Designer.cs
@@ -1405,7 +1405,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1372, 857);
+            this.ClientSize = new System.Drawing.Size(1924, 857);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.pnlLichChieu);
             this.Controls.Add(this.pnlDinhDang);
