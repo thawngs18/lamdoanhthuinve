a9cd1e1de73cb1fb506ec3c75d0d821e218b2103
Thắng Phan Đức <apgpdt10@gmail.com>
2024-12-08 00:39:00 +0700

    done ngay 12/8/2024

--- WindowsFormsApp8/Program.cs
+++ WindowsFormsApp8/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp8
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Seller());
+            Application.Run(new BanVe ());
         }
     }
 }
